b20fdf1f8700a479ea6c1af47821249d0442fed8
Rocky Breslow <rbreslow@azavea.com>
2020-01-04 21:44:59 -0500

    Calls to resource.addFile are now generated by cibuild.

--- src/speak/lua/autorun/speak.lua
+++ src/speak/lua/autorun/speak.lua
@@ -11,33 +11,6 @@ if SERVER then
 
     -- conf
     include("conf/tags.lua")
-
-    resource.AddFile("sound/speak/boing.mp3")
-    resource.AddFile("sound/speak/ding.mp3")
-    resource.AddFile("sound/speak/drop.mp3")
-    resource.AddFile("sound/speak/here_you_go.mp3")
-    resource.AddFile("sound/speak/hi.mp3")
-    resource.AddFile("sound/speak/knock_brush.mp3")
-    resource.AddFile("sound/speak/plink.mp3")
-    resource.AddFile("sound/speak/ta_da.mp3")
-    resource.AddFile("sound/speak/woah.mp3")
-    resource.AddFile("sound/speak/yoink.mp3")
-    resource.AddFile("sound/speak/wow.mp3")
-
-    resource.AddFile("resource/fonts/CoolveticaBk-Italic.ttf")
-    resource.AddFile("resource/fonts/CoolveticaBk-Regular.ttf")
-    resource.AddFile("resource/fonts/CoolveticaEl-Italic.ttf")
-    resource.AddFile("resource/fonts/CoolveticaEl-Regular.ttf")
-    resource.AddFile("resource/fonts/CoolveticaHv-Italic.ttf")
-    resource.AddFile("resource/fonts/CoolveticaHv-Regular.ttf")
-    resource.AddFile("resource/fonts/CoolveticaLt-Italic.ttf")
-    resource.AddFile("resource/fonts/CoolveticaLt-Regular.ttf")
-    resource.AddFile("resource/fonts/CoolveticaRg-Bold.ttf")
-    resource.AddFile("resource/fonts/CoolveticaRg-BoldItalic.ttf")
-    resource.AddFile("resource/fonts/CoolveticaRg-Italic.ttf")
-    resource.AddFile("resource/fonts/CoolveticaRg-Regular.ttf")
-    resource.AddFile("resource/fonts/CoolveticaUl-Italic.ttf")
-    resource.AddFile("resource/fonts/CoolveticaUl-Regular.ttf")
 end
 
 if CLIENT then
