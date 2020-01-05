8fb65e35762669521ec92addc15f1d37a10dd59a
Rocky Breslow <rbreslow@azavea.com>
2020-01-04 22:04:47 -0500

    Remove legacy addon.

--- speak_garrysmod/lua/conf/emoticons.lua
+++
@@ -1,6 +0,0 @@
---[[ Misc ]]
-Emoticons:Define(":spider1:", "https://emoji.slack-edge.com/T0887PSCQ/spider1/86aabace07464d38.png")
-Emoticons:Define(":spider2:", "https://emoji.slack-edge.com/T0887PSCQ/spider2/08d8067f3900b90e.png")
-Emoticons:Define(":rolf:", "https://emoji.slack-edge.com/T0887PSCQ/rolf/60eed8eba015ab14.png")
-
-Emoticons:Define(":B1:", "http://orig11.deviantart.net/e380/f/2014/100/3/8/b1_in_hd_by_tomajko-d7dz8iy.png")
--- speak_garrysmod/lua/conf/tags.lua
+++
@@ -1,6 +0,0 @@
-Tags:DefineForSteamID64(76561198058648898, {
-    Color(255, 0, 0),
-    "~speak creator~"
-})
-
-Tags:UpdateClients()
