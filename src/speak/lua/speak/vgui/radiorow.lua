6741baa611c141a8e872a121d7b24ddee4bda577
Rocky Breslow <rbreslow@azavea.com>
2020-04-11 21:51:36 -0400

    Remove verbose calls to render.SetStencilReferenceValue

--- src/speak/lua/speak/vgui/radiorow.lua
+++ src/speak/lua/speak/vgui/radiorow.lua
@@ -87,7 +87,6 @@ function PANEL:Paint(w, h)
     -- Reset everything to known good
     render.SetStencilWriteMask(0xFF)
     render.SetStencilTestMask(0xFF)
-    render.SetStencilReferenceValue(0)
     render.SetStencilPassOperation(STENCIL_KEEP)
     render.SetStencilZFailOperation(STENCIL_KEEP)
     render.ClearStencil()
